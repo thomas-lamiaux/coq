04f6b2fbeb43fba51986333112768313f34211c7
Thomas Lamiaux <85848641+thomas-lamiaux@users.noreply.github.com>
2025-04-26 02:42:53 +0200

    Add a module Ltac1CompatibilityNotations to Ltac2/user-contrib/

---
+++ user-contrib/Ltac2/Ltac1CompatNotations.v
@@ -0,0 +1,20 @@
+(************************************************************************)
+(*         *      The Rocq Prover / The Rocq Development Team           *)
+(*  v      *         Copyright INRIA, CNRS and contributors             *)
+(* <O___,, * (see version control and CREDITS file for authors & dates) *)
+(*   \VV/  **************************************************************)
+(*    //   *    This file is distributed under the terms of the         *)
+(*         *     GNU Lesser General Public License Version 2.1          *)
+(*         *     (see LICENSE file for the text of the license)         *)
+(************************************************************************)
+
+
+(* This file contains Ltac2 Notations reproducing Ltac1 parsing of tactics,
+   that can be harmful to parsing, and produce bad error messages.
+   They should hence only be imported to port proof script written in Ltac1,
+   or if they are really wanted.
+*)
+
+Require Import Ltac2.Init Ltac2.Std.
+
+Ltac2 Notation "transitivity" c(constr) := Std.transitivity c.
