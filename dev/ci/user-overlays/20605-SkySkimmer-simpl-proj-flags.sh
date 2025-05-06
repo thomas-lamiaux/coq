d93f4601a84205bde91269e2d0909ab4cd66717f
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2025-05-06 14:51:04 +0200

    Overlays for record field flags

---
+++ dev/ci/user-overlays/20605-SkySkimmer-simpl-proj-flags.sh
@@ -0,0 +1,7 @@
+overlay elpi https://github.com/SkySkimmer/coq-elpi simpl-proj-flags 20605
+
+overlay metarocq https://github.com/SkySkimmer/metarocq simpl-proj-flags 20605
+
+overlay lean_importer https://github.com/SkySkimmer/rocq-lean-import simpl-proj-flags 20605
+
+overlay coq_lsp https://github.com/SkySkimmer/coq-lsp simpl-proj-flags 20605
