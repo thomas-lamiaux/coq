9d78508adb47b4c0482d5558cabf6d9a4be42dbb
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2025-09-24 16:42:36 +0200

    Add overlays.

---
+++ dev/ci/user-overlays/21110-ppedrot-proof-solve-explicit-env.sh
@@ -0,0 +1,9 @@
+overlay coq_lsp https://github.com/ppedrot/coq-lsp proof-solve-explicit-env 21110
+
+overlay equations https://github.com/ppedrot/Coq-Equations proof-solve-explicit-env 21110
+
+overlay mtac2 https://github.com/ppedrot/Mtac2 proof-solve-explicit-env 21110
+
+overlay rewriter https://github.com/ppedrot/rewriter proof-solve-explicit-env 21110
+
+overlay tactician https://github.com/ppedrot/coq-tactician proof-solve-explicit-env 21110
