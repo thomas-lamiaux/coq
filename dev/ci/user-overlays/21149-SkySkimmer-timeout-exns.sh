c45df347e7febdae180bc4a4034cbe297079b8d5
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2025-10-02 14:39:15 +0200

    overlays

---
+++ dev/ci/user-overlays/21149-SkySkimmer-timeout-exns.sh
@@ -0,0 +1,5 @@
+overlay lean_importer https://github.com/SkySkimmer/rocq-lean-import timeout-exns 21149
+
+overlay waterproof https://github.com/SkySkimmer/coq-waterproof timeout-exns 21149
+
+overlay tactician https://github.com/SkySkimmer/coq-tactician timeout-exns 21149
