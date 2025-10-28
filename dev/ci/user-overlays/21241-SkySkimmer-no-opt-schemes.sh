15280556137aa8dd06652aa866a0bfd1d878b2b9
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2025-10-28 15:18:30 +0100

    Stop "optimizing" schemes by reusing preexisting schemes in Type for other sorts

---
+++ dev/ci/user-overlays/21241-SkySkimmer-no-opt-schemes.sh
@@ -0,0 +1,11 @@
+overlay stdlib https://github.com/SkySkimmer/stdlib no-opt-schemes 21241
+
+overlay bbv https://github.com/SkySkimmer/bbv no-opt-schemes 21241
+
+overlay kami https://github.com/SkySkimmer/kami no-opt-schemes 21241
+# Make PRs against https://github.com/mit-plv/kami base branch rv32i
+
+overlay bedrock2 https://github.com/SkySkimmer/bedrock2 no-opt-schemes 21241
+# Make PRs against https://github.com/mit-plv/bedrock2 base branch master
+
+overlay coq_dpdgraph https://github.com/SkySkimmer/coq-dpdgraph no-opt-schemes 21241
