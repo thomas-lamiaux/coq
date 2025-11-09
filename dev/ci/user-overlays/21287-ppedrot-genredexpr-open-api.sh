3bc82cbf25ffc94eb1f0726e0ff91454581dbad2
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2025-11-09 20:33:48 +0100

    Add overlays.

---
+++ dev/ci/user-overlays/21287-ppedrot-genredexpr-open-api.sh
@@ -0,0 +1,5 @@
+overlay coq_lsp https://github.com/ppedrot/coq-lsp genredexpr-open-api 21287
+
+overlay elpi https://github.com/ppedrot/coq-elpi genredexpr-open-api 21287
+
+overlay tactician https://github.com/ppedrot/coq-tactician genredexpr-open-api 21287
