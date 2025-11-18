rocq-prover/rocq

overlays for moved flags

dev/ci/user-overlays/20606-SkySkimmer-clean-flags.sh
@@ -1,0 +1,1 @@
+overlay vscoq https://github.com/SkySkimmer/vscoq clean-flags 20606

thomas-lamiaux/coq

dev/ci/user-overlays/20606-SkySkimmer-clean-flags.sh
@@ -1,0 +1,1 @@
+overlay vsrocq https://github.com/SkySkimmer/vscoq clean-flags 20606