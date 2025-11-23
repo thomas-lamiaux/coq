b06a876948d0087458a85b01a2d60d61ae69ba6c
Johann Rosain <johann.rosain@ens-lyon.org>
2025-11-23 19:52:56 +0100

    Add overlays for rocq-prover/rocq#21195

---
+++ dev/ci/user-overlays/21195-jrosain-elimination-constraints.sh
@@ -0,0 +1,23 @@
+overlay equations https://github.com/jrosain/Rocq-Equations elimination-constraints 21195
+
+overlay coq_dpdgraph https://github.com/jrosain/rocq-dpdgraph elimination-constraints 21195
+
+overlay lean_importer https://github.com/jrosain/rocq-lean-import elimination-constraints 21195
+
+overlay mtac2 https://github.com/jrosain/Mtac2 elimination-constraints 21195
+
+overlay paramcoq https://github.com/jrosain/paramcoq elimination-constraints 21195
+
+overlay rewriter https://github.com/jrosain/rewriter elimination-constraints 21195
+
+overlay tactician https://github.com/jrosain/rocq-tactician elimination-constraints 21195
+
+overlay waterproof https://github.com/jrosain/rocq-waterproof elimination-constraints 21195
+
+overlay coq_lsp https://github.com/jrosain/rocq-lsp elimination-constraints 21195
+
+overlay metarocq https://github.com/jrosain/metarocq elimination-constraints 21195
+
+overlay elpi https://github.com/TDiazT/coq-elpi elimination-constraints 21195
+
+overlay itauto https://gitlab.inria.fr/tdiaztro/itauto elimination-constraints 21195
