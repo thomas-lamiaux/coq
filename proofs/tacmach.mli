f090936e710e5371cbe1373aed96fd026669c662
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2025-11-26 15:37:01 +0100

    Deprecate Tacred.pf_hnf_constr.

--- proofs/tacmach.mli
+++ proofs/tacmach.mli
@@ -54,6 +54,7 @@ val pf_nf_concl : Proofview.Goal.t -> types
 [@@ocaml.deprecated "(9.2) Use Reductionops.nf_evar"]
 
 val pf_hnf_constr : Proofview.Goal.t -> constr -> types
+[@@ocaml.deprecated "(9.2) Use Tacred.hnf_constr"]
 
 val pf_hnf_type_of : Proofview.Goal.t -> constr -> types
 [@@ocaml.deprecated "(9.2) Use Reductionops.whd_all and Retyping.get_type_of"]
