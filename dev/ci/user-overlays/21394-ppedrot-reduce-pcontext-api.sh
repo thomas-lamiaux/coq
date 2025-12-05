bdf1ef7c21ea266d545c088fa2cb70cee4a52528
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2025-12-05 08:43:36 +0100

    Add overlays.

---
+++ dev/ci/user-overlays/21394-ppedrot-reduce-pcontext-api.sh
@@ -0,0 +1,11 @@
+overlay elpi https://github.com/ppedrot/coq-elpi reduce-pcontext-api 21394
+
+overlay equations https://github.com/ppedrot/Coq-Equations reduce-pcontext-api 21394
+
+overlay mtac2 https://github.com/ppedrot/Mtac2 reduce-pcontext-api 21394
+
+overlay paramcoq https://github.com/ppedrot/paramcoq reduce-pcontext-api 21394
+
+overlay rewriter https://github.com/ppedrot/rewriter reduce-pcontext-api 21394
+
+overlay metarocq https://github.com/ppedrot/metarocq reduce-pcontext-api 21394
