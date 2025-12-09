d04e296236f2a8523719c1013d16d9f0f7631a62
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2025-12-09 08:27:56 +0100

    Fix native conversion of polymorphic code after #21405.
    The code for the VM was correctly adapted in that PR, but not the native one.
    It was suffering from the same problem, i.e. raising assertion failures
    when faced with level variables. We adapt the code in the same way, by
    discriminating between toplevel and constant compilation.

--- test-suite/bugs/bug_21405.v
+++ test-suite/bugs/bug_21405.v
@@ -5,3 +5,6 @@ Definition U@{u} : Type@{u+1} := Type@{u}.
 
 Definition anomaly := (tt <: T).
 Definition anomaly' := (Type <: U).
+
+Definition native_anomaly := (tt <<: T).
+Definition native_anomaly' := (Type <<: U).
