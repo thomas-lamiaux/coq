24eefc14e4e18baa19bef07b09b9415c0a91db7b
Yann Leray <yann.leray@inria.fr>
2026-01-07 15:36:06 +0100

    cbn/simpl recursive calls in RR argument now with properly updated env

--- pretyping/evarconv.ml
+++ pretyping/evarconv.ml
@@ -115,7 +115,7 @@ let eval_flexible_term ts env evd c sk =
             try
             let r = match lookup_rewrite_rules c env with r -> r | exception Not_found -> assert false in
             let rhs_stack = Reductionops.apply_rules
-              (whd_betaiota_deltazeta_for_iota_state ts env evd) env evd u r sk
+              (fun ctx -> whd_betaiota_deltazeta_for_iota_state ts (push_rel_context ctx env) evd) env evd u r sk
             in
             Some rhs_stack
           with PatternFailure -> None
